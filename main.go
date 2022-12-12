c4990ad532044dc58ad26583176733468a0be069
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-12-12 18:20:42 +0700

    add employees query

--- main.go
+++ main.go
@@ -26,6 +26,14 @@ func ConnectDB() (*sql.DB, error) {
 	return db, nil
 }
 
+type Employee struct {
+	ID int `sql:"id"`
+	Name string `sql:"name"`
+	age int `sql:"age"`
+	address string `sql:"address"`
+	salary float32 `sql:"salary"`
+}
+
 func main() {
 	db, err := ConnectDB()
 	if err != nil {
@@ -50,18 +58,38 @@ func main() {
 	// `)
 
 	// insert employees data
-	_, err = db.Exec(`
-		INSERT INTO Employees
-		VALUES
-		(1, 'Rizki', 25, 'Jl. Kebon Jeruk', 2000000),
-		(2, 'Andi', 27, 'Jl. Kebon Sirih', 3000000),
-		(3, 'Budi', 30, 'Jl. Kebon Melati', 4000000),
-		(4, 'Caca', 32, 'Jl. Kebon Anggrek', 5000000),
-		(5, 'Deni', 35, 'Jl. Kebon Mawar', 6000000)
-	`)
+	// _, err = db.Exec(`
+	// 	INSERT INTO Employees
+	// 	VALUES
+	// 	(1, 'Rizki', 25, 'Jl. Kebon Jeruk', 2000000),
+	// 	(2, 'Andi', 27, 'Jl. Kebon Sirih', 3000000),
+	// 	(3, 'Budi', 30, 'Jl. Kebon Melati', 4000000),
+	// 	(4, 'Caca', 32, 'Jl. Kebon Anggrek', 5000000),
+	// 	(5, 'Deni', 35, 'Jl. Kebon Mawar', 6000000)
+	// `)
+
+	// scan employees data
+	rows, err := db.Query("SELECT * FROM Employees")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Table datas inserted!")
+	var employees []Employee
+
+	for rows.Next() {
+		var employee Employee 
+
+		err := rows.Scan(&employee.ID, &employee.Name, &employee.age, &employee.address, &employee.salary)
+		if err != nil {
+			panic(err)
+		}
+
+		employees = append(employees, employee)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(employees)
 }
